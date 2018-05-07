b683a9d3be4ebf9d692e91cb534cc0b07eaef765
Saix_93 <r.torresgonzalez.93@gmail.com>
2018-05-07 15:47:11 +0200

    Renombra App a Header para la organización de la página

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './Header';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('header'));
+// ReactDOM.render(<Main />, document.getElementById('main'));
 registerServiceWorker();
