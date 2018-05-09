2f46f51370475b4d1f64ce0423b149646a317595
Saix_93 <r.torresgonzalez.93@gmail.com>
2018-05-09 17:48:06 +0200

    Añade botón para ir a la siguiente página

--- src/Skills.js
+++ src/Skills.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+// Components
+import BodyLink from './BodyLink.js';
+import Work from './Work.js';
+
 class Skills extends Component {
   render() {
     return (
       <div>
-        <h1 className="MenuItem-title">Contact</h1>
+        <h1 className="MenuItem-title">Skills</h1>
+        <BodyLink to="Work" text="My Work >" onClick={() => this.props.onClick("Work", Work)}/>
       </div>
     );
   }
--- src/Work.js
+++ src/Work.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+// Components
+import BodyLink from './BodyLink.js';
+import Contact from './Contact.js';
+
 class Work extends Component {
   render() {
     return (
       <div>
-        <h1 className="MenuItem-title">Contact</h1>
+        <h1 className="MenuItem-title">My Work</h1>
+        <BodyLink to="Contact" text="How to Contact me >" onClick={() => this.props.onClick("Contact", Contact)}/>
       </div>
     );
   }
