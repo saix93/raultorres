06ea27b74a40a085f78208d51e94d00a4c344e7e
Saix_93 <r.torresgonzalez.93@gmail.com>
2018-05-11 01:42:58 +0200

    Modifica app.js para permitir la recarga de página utilizando una URL de react

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict';
-var http	      = require('http');
-var express 		= require('express');
-var app 		    = express();
+const http	      = require('http');
+const express 		= require('express');
+const app 		    = express();
+const path        = require('path')
 
-var webServer = http.createServer(app).listen(80);
+const port = 8080;
+
+var webServer = http.createServer(app).listen(port);
 
 function start() {
-  app.use('/', express.static('public'));
+  app.use(express.static(__dirname + '/public'));
+
+  app.get('*', function (request, response){
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  });
 }
 
 start();
